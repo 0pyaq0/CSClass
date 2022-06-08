7a1760e2a2c13a6b51a69fc322a979bbc27d1119
심이진 <s2011@e-mirim.hs.kr>
2022-06-08 10:15:02 +0900

    #12 22-6 생성자 : 정적 생성자 딱 1번 클래스가 언급(메모리 로드)되었을 때 호출됨

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSClass31;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -121,6 +122,12 @@ namespace CSClass
             Console.WriteLine(Product.counter + "개가 생성되었습니다.");
 
             SingleTon mySingle = SingleTon.getInstance();
+
+            Console.WriteLine("첫 번째 위치");
+            Sample sample = new Sample();
+            Console.WriteLine("두 번째 위치");
+            Console.WriteLine(Sample.value);
+            Console.WriteLine("세 번째 위치");
         }
     }
 }
